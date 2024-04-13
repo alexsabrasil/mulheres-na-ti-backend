bf06f52a06549e9ee102e24f9d53c55de58ee297
alexsabrasil <alexsa.tecnolog@gmail.com>
2024-04-13 10:35:52 -0300

    insere a nova versão do projeto integrado com o mongodb

--- mulheres.js
+++ mulheres.js
@@ -1,36 +1,97 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express") //aqiui estou iniciando o express
+const router = express.Router() //aqui estou configurando a primeira parte da rota
+const cors = require('cors')//Aqui estou trazeno o pacote cors que permite comsumir essa API no front-end
 
-const app = express()
-const porta = 3333
+const conectaBancoDeDados = require('./bancoDeDados') //Aqui estou ligando ao arquivo banco de dados
+conectaBancoDeDados() //Aqui estou chamando a função, que conecta o banco de dados
 
+const Mulher = require('./mulherModel')
 
-const mulheres = [
-    {
-        nome: 'Alexsandra Tavares', 
-        imagem: 'https://im.ge/i/1707252172189.WGuHYG',
-        minibio: 'Neuropsicopedagoga e Engenheira de Dados' ,
-    },
-    {
-        nome: 'Alexsandra Tavares', 
-        imagem: 'https://im.ge/i/1707252172189.WGuHYG',
-        minibio: 'Neuropsicopedagoga e Engenheira de Dados' ,
-    },
-    {
-        nome: 'Alexsandra Tavares', 
-        imagem: 'https://im.ge/i/1707252172189.WGuHYG',
-        minibio: 'Neuropsicopedagoga e Engenheira de Dados' ,
-    },
-]
+const app = express() //aqui estou iniciando o app
+app.use(express.json())//Tratando as requisições
+app.use(cors())
 
-function mostraMulheres(request, response) {
-response.json(mulheres)
+const porta = 3333//aqui estou criando a porta
+
+//GET
+async function mostraMulheres(request, response) {
+    try {
+        const mulheresVindasDoBancoDeDados = await Mulher.find()
+
+        response.json(mulheresVindasDoBancoDeDados)
+    }catch (erro) {
+        console.log(erro)
+    }
 
 }
 
+//POST
+async function criaMulher(request, response) {
+    const novaMulher = new Mulher({
+        nome: request.body.nome,
+        imagem: request.body.imagem,
+        minibio: request.body.minibio,
+        citacao: request.body.citacao 
+    })
+
+    try {
+        const mulherCriada = await novaMulher.save() 
+        response.status(201).json(mulherCriada)
+    }catch (erro) {
+        console.log(erro)
+    }
+    
+}
+
+//PATCH
+async function corrigeMulher(request, response) {
+    try {
+        const mulherEncontrada = await Mulher.findById(request.params.id)
+
+        if (request.body.nome) {
+            mulherEncontrada.nome = request.body.nome
+        }
+    
+        if (request.body.minibio) {
+            mulherEncontrada.minibio = request.body.minibio
+        }
+    
+        if (request.body.imagem) {
+            mulherEncontrada = request.body.imagem
+        }
+
+        if (request.body.citacao) {
+            mulherEncontrada = request.body.citacao
+        }
+
+        const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+
+        response.json(mulherAtualizadaNoBancoDeDados)
+
+    } catch (erro) {
+        console.log(erro)
+    }   
+}
+
+//DELETE
+async function deletaMulher(request, response) {
+    try {
+        await Mulher.findByIdAndDelete(request.params.id)
+        response.json({mensagem: 'Mulher deletada com sucesso!'})
+
+    } catch(erro) {
+        console.log(erro)
+    }
+}
+
+app.use(router.get('/mulheres', mostraMulheres)) // configurei rota GET /mulheres
+app.use(router.post('/mulheres', criaMulher)) //Configurei rota POST /mulheres
+app.use(router.patch('/mulheres/:id', corrigeMulher)) //Configurei rota PATCH /mulheres/:id
+app.use(router.delete('/mulheres/:id', deletaMulher)) //Configurei rota DELETE /mulheres
+
+//Função PORTA
     function mostraPorta() {
         console.log('Servidor criado e rodando na porta' , porta)
 }
 
-app.use(router.get('/mulheres', mostraMulheres))
-app.listen(porta, mostraPorta)
+app.listen(porta, mostraPorta) //servidor ouvindo a porta
--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require("express")
+const { v4: uuidv4 } = require('uuidv4')
+const router = express.Router()
 
 const app = express()
 const porta = 3333
 
-    function mostraPorta() {
-        console.log('Servidor criado e rodando na porta' , porta)
+//POST
+function criaMulher(req, response) {
+    const novaMulher = {
+        id: uuidv4(),
+        nome: req.body.nome,
+        imagem: req.body.imagem,
+        minibio: req.body.minibio
+    }
+    
+    mulheres.push(novaMulher)
+
+    response.json(mulheres)
+}
+function mostraPorta() {
+    console.log("Servidor criado e rodando na porta ", porta)
 }
 
-//chamar a função, com os parenteses
-//mostraPorta()
+app.use(router.post('/mulheres', criaMulher))
+
+app.listen(porta, mostraPorta)
+//chamar a função, com os parenteses, mostraPorta()
 //Execcutar no terminal digita os comandos e testar o código: node server.js
 //Resposta no terminal: Servidor criado e rodando na porta 3333
 //app.Listen(porta, mostraPorta), no terminal digita node server.js. Para rodar o servidor
-app.listen(porta, mostraPorta)
